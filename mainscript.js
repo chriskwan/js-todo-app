15dd6da7d00a5fddde7b9c12c12d93b4baadcc37
Chris Kwan <chriswkwan@gmail.com>
2014-11-12 23:33:52 -0500

    Add loading of tasks after getting tasks from JSON file with an AJAX call

--- mainscript.js
+++ mainscript.js
@@ -1,13 +1,80 @@
-(function(){
+$(document).ready(function(){
 
-	attachEvents();
+	main();
+
+	function main() {
+		attachEvents();		
+	}
 
 	function attachEvents() {
+		attachLoadTasks();
 		attachUserNameHandler();
 		attachMoreButtonHandler();
 	}
 
-	function attachUserNameHandler(){
+	function attachLoadTasks() {
+		//jQuery practice
+		$("#loadTasksLink").click(function(event){
+			//using success
+			// $.ajax({
+			// 	url: "http://localhost:8080/tasks.json",
+			// 	success: function(data){
+			// 		var mydata = data;
+			// 	}
+			// });
+
+			//using done
+			$.ajax({
+				url: "http://localhost:8080/tasks.json",
+				type: "GET", //POST gets 404
+				data: {
+					cats: 12, 
+					lions: false
+				}
+			}).done(function(data){
+				//add new tasks
+				var $tasks = $('#tasklist');
+				for (var i=0; i<data.length; i++) {
+					var item = data[i];
+					var $newTask = getTaskElement(
+						item.isDone,
+						item.name,
+						item.description
+					);
+					$tasks.append($newTask);
+				}
+			});
+		})
+	}
+
+	function getTaskElement(isTaskCompleted, taskName, taskDetails) {
+		var $newTask = $('<li></li>');
+		$newTask.addClass('task');
+		
+		var $taskHeader = $('<div></div>');
+		$taskHeader.addClass('task-header');
+		$taskHeader.append($('<input>', {
+			type: "checkbox",
+			checked: isTaskCompleted
+		}));
+		$taskHeader.append($('<span></span>').text(taskName));
+		$taskHeader.append(
+			$('<button></button>')
+			.text("More")
+			.addClass('more-button show-more')
+		);
+		$newTask.append($taskHeader);
+
+		var $taskDetails = $('<div></div>');
+		$taskDetails.addClass('task-details');
+		$taskDetails.addClass('is-hidden');
+		$taskDetails.append($('<p></p>').text(taskDetails));
+		$newTask.append($taskDetails);
+
+		return $newTask;
+	}
+
+	function attachUserNameHandler() {
 		var userNames = document.querySelectorAll(".user-name");
 		var complimentName = function() {
 			alert("You've got a nice name!");
@@ -50,4 +117,4 @@
 		}
 	}
 
-})()
+});
